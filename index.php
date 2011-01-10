e82a9f3aec7b9511451efdca913738fe4d2959ab
overlord_tm <overlordtm@gmail.com>
2011-01-11 00:05:31 +0100

    Tue Jan 11 00:05:31 CET 2011

--- index.php
+++ index.php
@@ -2,37 +2,28 @@
 <head> 
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-2" /> 
 </head>
-
 <?php 
 
 if(isset($_POST["besedilo"])) {
 	//$ncen = preg_match_all ('/([\d\.\,]+[\-]*\s*(€|EUR|evr|e\s|&#8364;|e\.|e\,|eu))/i',$_POST["besedilo"], $vsecene);
 
 	// pogledamo za skupno ceno
-    $skupnaCena = preg_match_all ('/(?:sku|oba|oboje|vse)[a-z\s:=\,~]+\s*([\d\,\.]+)\s*(?:€|EUR|evr|&#8364;|e[\,\.\s]|eu)/i',$_POST["besedilo"], $vsecene);
+    $skupnaCena = preg_match_all ('/(?:skup|oba|oboje|vse|komplet)[a-z\s\-\,:=~>]+\s+([\d\,\.]+)\s*(?:€|EUR|evr|&#8364;|e[\,\.\s]|eu)/i',$_POST["besedilo"], $vsecene);
 	if ($skupnaCena) {
 		echo "Nasel skupno ceno!<br>";
 		print_r($vsecene);
     	$okvirnacena = (max(array_map('floatval',$vsecene[1])));	
     } else {
-    	echo "Iscem take z prefixom ce<br>";
-    	// pogledamo za cene ki imajo prefix "ce". Zato da se izognemo raznim oznakam izdelkov ala WRT1000E
-    	$ncen = preg_match_all ('/(?:ce)[a-z\s:=~]+\s*([\d\,\.]+)\s*(?:€|EUR|evr|&#8364;|e[\,\.\s]|eu)/i',$_POST["besedilo"], $vsecene);
-    	if($ncen) {
-    		echo "Nasel take z prefixom ce<br>";
-    		print_r($vsecene);
-    		$okvirnacena = (max(array_map('floatval',$vsecene[1])));	
-    	} else {
-    		echo "Obupano iskanje vsega kar zgleda kot cena<br>";
-    		$ncen = preg_match_all ('/([\d\,\.]+)\s*(?:€|EUR|evr|&#8364;|e[\,\.\s]|eu)/i',$_POST["besedilo"], $vsecene);
-    		if($ncen) {
-    			print_r($vsecene);
-    			$okvirnacena = (max(array_map('floatval',$vsecene[1])));	
-    		} else {
-    			$okvirnacena = -1.0;	
-    		}
-    	}
-    }
+ 		echo "Failover<br>";
+ 		$ncen = preg_match_all ('/[\s=:>\(]+([\d\.\,]+)[\-\s]*(?:€|EUR|evr|e[\,\.\s]|&#8364;|eu)(\)){0,1}/i',$_POST["besedilo"], $vsecene);
+		print_r($vsecene);
+ 		if($ncen) {
+ 			preg_match_all ('/(?:trgovi)[a-z\s\-\,:=~>]+\s+([\d\.\,]+)[\-\s]*(?:€|EUR|evr|e[\,\.\s]|&#8364;|eu)(\)){0,1}/i',$_POST["besedilo"], $trgovinacene);
+			$okvirnacena = array_sum(array_map('floatval',$vsecene[1])) - array_sum(array_map('floatval',$trgovinacene[1]));
+		} else {
+   			$okvirnacena = -1.0;	
+   		}	
+   	}
 	echo "<pre>" . $_POST["besedilo"] . "</pre>";
 	echo "<br> Okvirna cena je: " . $okvirnacena;
 } else {
