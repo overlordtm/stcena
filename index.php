cad7ee4de47be4b110bd900e2b7f46649827c5ff
overlord_tm <overlordtm@gmail.com>
2011-01-11 00:49:19 +0100

    Tue Jan 11 00:49:19 CET 2011

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sl" lang="sl" dir="ltr"> 
 <head> 
-	<meta http-equiv="content-type" content="text/html; charset=iso-8859-2" /> 
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
 </head>
 <?php 
 
-if(isset($_POST["besedilo"])) {
-	//$ncen = preg_match_all ('/([\d\.\,]+[\-]*\s*(€|EUR|evr|e\s|&#8364;|e\.|e\,|eu))/i',$_POST["besedilo"], $vsecene);
-
+function cena($string) {
 	// pogledamo za skupno ceno
-    $skupnaCena = preg_match_all ('/(?:skup|oba|oboje|vse|komplet)[a-z\s\-\,:=~>]+\s+([\d\,\.]+)\s*(?:€|EUR|evr|&#8364;|e[\,\.\s]|eu)/i',$_POST["besedilo"], $vsecene);
+    $skupnaCena = preg_match_all ('/(?:skup|oba|oboje|vse|komplet)[a-z\s\-\,:=~>]+\s+([\d\,\.]+)\s*(?:€|EUR|evr|&#8364;|e[\,\.\s]|eu)/i',$string, $vsecene);
 	if ($skupnaCena) {
-		echo "Nasel skupno ceno!<br>";
-		print_r($vsecene);
+		//echo "Nasel skupno ceno!<br>";
+		//print_r($vsecene);
     	$okvirnacena = (max(array_map('floatval',$vsecene[1])));	
     } else {
- 		echo "Failover<br>";
- 		$ncen = preg_match_all ('/[\s=:>\(]+([\d\.\,]+)[\-\s]*(?:€|EUR|evr|e[\,\.\s]|&#8364;|eu)(\)){0,1}/i',$_POST["besedilo"], $vsecene);
-		print_r($vsecene);
+ 		//echo "Failover<br>";
+ 		$ncen = preg_match_all ('/[\s=:>\(]+([\d\.\,]+)[\-\s]*(?:€|EUR|evr|e[\,\.\s]|&#8364;|eu)(\)){0,1}/i',$string, $vsecene);
+		//print_r($vsecene);
  		if($ncen) {
- 			preg_match_all ('/(?:trgovi)[a-z\s\-\,:=~>]+\s+([\d\.\,]+)[\-\s]*(?:€|EUR|evr|e[\,\.\s]|&#8364;|eu)(\)){0,1}/i',$_POST["besedilo"], $trgovinacene);
+ 			preg_match_all ('/(?:trgovi)[a-z\s\-\,:=~>]+\s+([\d\.\,]+)[\-\s]*(?:€|EUR|evr|e[\,\.\s]|&#8364;|eu)(\)){0,1}/i',$string, $trgovinacene);
 			$okvirnacena = array_sum(array_map('floatval',$vsecene[1])) - array_sum(array_map('floatval',$trgovinacene[1]));
 		} else {
+			
+			$ncen = preg_match_all ('/(?:)[\s=:>\(]+([\d\.\,]+)[\-\s]*(?:€|EUR|evr|e[\,\.\s]|&#8364;|eu)(\)){0,1}/i',$string, $vsecene);
+			
    			$okvirnacena = -1.0;	
    		}	
    	}
+   	return $okvirnacena;
+}
+
+if(isset($_POST["besedilo"])) {
+	//$ncen = preg_match_all ('/([\d\.\,]+[\-]*\s*(€|EUR|evr|e\s|&#8364;|e\.|e\,|eu))/i',$_POST["besedilo"], $vsecene);
+	
+	$okvirnacena = cena($_POST["besedilo"]);
+		
 	echo "<pre>" . $_POST["besedilo"] . "</pre>";
 	echo "<br> Okvirna cena je: " . $okvirnacena;
-} else {
+} else if($_SERVER["SCRIPT_NAME"] == "/st/index.php") {
 	?>
 	<form method="post">
 		<textarea name="besedilo" cols="80" rows="30"></textarea>
---
+++ test.php
@@ -0,0 +1,41 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sl" lang="sl" dir="ltr"> 
+<head> 
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+</head>
+
+<pre>
+<?php 
+
+include 'index.php';
+
+$lines = file('d1f5630a0144ee4cb8f9241795b2acbf');
+
+$counter = 0;
+$n = 0;
+
+// Loop through our array, show HTML source as HTML source; and line numbers too.
+foreach ($lines as $line_num => $line) {
+	
+	$ok = preg_match_all ('/[0-9]{6}[\s]+P:/i',$line, $vsecene);
+	
+	if($ok) {
+		$line2 = str_replace(array("\r\n", "\n", "\r"), " ", $line);
+	    echo "Line #<b>{$line_num}</b> : " . $line2 . "<br />\n";
+	    $okvirnacena = cena($line2);
+	    echo "Okvirna cena: " . $okvirnacena . "<br>\n";
+	    
+	    $n++;
+	    if($okvirnacena > 0) {
+	    	$counter++;
+	    }
+	}
+}
+
+?>
+</pre>
+<?php 
+
+echo "uspesno ocenjenih $counter oglasov od $n!<br>\n";
+echo "razmerje je" . ((float)$counter/(float)$n);
+
+?>
